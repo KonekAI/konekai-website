edba7689a99a25980810d3524846295984b5d70a
crystal <noelani.villanueva@gmail.com>
2021-05-08 14:07:19 -0700

    done

--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ import FeatureTwo from "./components/FeatureTwo";
 import FeatureThree from "./components/FeatureThree";
 import Team from "./components/Team";
 import { useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 function App() {
-  useEffect(()=>{
-    axios.get(`https://konek-ai.herokuapp.com/oauthButton`,{
-      target: 'https://konek-ai.herokuapp.com',
-      logLevel: 'debug',
-      changeOrigin: true
-  })
-    .then(res=>res.json());
-  })
   return (
     <>
       <Container>
