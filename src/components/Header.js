7b971eeb56b1f935630c2d8c6bcf77403100bd7e
arjun006 <arjun.devakumar66@gmail.com>
2021-05-09 04:16:01 -0400

    small changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -111,8 +111,8 @@ export default function Header() {
                 <a href={uri}>
                   <img
                     alt="Add to Slack"
-                    height="40"
-                    width="139"
+                    height="100%"
+                    width="150"
                     src="https://platform.slack-edge.com/img/add_to_slack.png"
                     srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
                   />
